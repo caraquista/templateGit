b904c8f1f8ce802b3f1ae1860d1729186a478eb7
Daniel Diaz <daniel.diaz@rokk3rlabs.com>
2022-11-16 11:44:47 -0500

    cambios en number.js

--- numbers.js
+++ numbers.js
@@ -2,7 +2,7 @@ function clickNumber (number) {
     // USUARIO 1
     // console.log('Hola', number);
     // USUARIO 2
-    // const value = document.getElementById('result')
+    const value = document.getElementById('result')
     // USUARIO 3
     // console.log('value', value);
     // USUARIO 4
